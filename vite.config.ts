5eeb1636089cc93052f8e311f201649e39ce793f
sea <cnareekarn@gmail.com>
2026-09-06 15:34:27 +0700

    fix: Dynamic base path in Vite for seamless localhost dev and GitHub Pages production

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/json-to-flutter-model-web/',
+  base: command === 'build' ? '/json-to-flutter-model-web/' : '/',
   server: {
     host: '0.0.0.0',
     port: 3000,
     open: false,
   }
-});
+}));
